6dfc10507bbd44bedd5321c4f080b5bb3e8367aa
Soren <soren@aethre.co>
2022-01-18 08:27:32 -0800

    grammar

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ const Home: NextPage = () => {
                 <img src="/robotx.svg" alt="FRC Team 6962: RobotX" className="w-100" />
             </h1>
             <p>
-                RobotX is a FRC team of aspiring, STEM passionate students. We&apos;re based in Khan
-                Lab School, in Mountain View, California where hands-on-learning and passion-driven
-                work are encouraged. We aim to inspire and expose others to the exploration of
-                science, mathematics, computer science, and technology; represent our self-driven
-                school in FRC competitions; and share our experiences and knowledge with the rest of
-                our community.
+                RobotX is an FRC team of aspiring, STEM passionate students. We&apos;re based in
+                Khan Lab School, in Mountain View, California where hands-on-learning and
+                passion-driven work are encouraged. We aim to inspire and expose others to the
+                exploration of science, mathematics, computer science, and technology; represent our
+                self-driven school in FRC competitions; and share our experiences and knowledge with
+                the rest of our community.
             </p>
         </div>
     );
