18430b75805d4c7f16724f84490980518e92c13e
Soren <soren@aethre.co>
2022-01-20 20:19:30 -0800

    Next.js image background

---
+++ components/imageBackground.tsx
@@ -0,0 +1,40 @@
+import Image, { ImageProps } from 'next/image';
+
+function ImageBackground({
+    className,
+    children,
+    imageClassName,
+    ...imageProps
+}: {
+    className?: string;
+    children?: React.ReactNode | React.ReactNode[];
+    imageClassName?: string;
+} & ImageProps) {
+    return (
+        <>
+            <div className="grid grid-cols-1 grid-rows-1 w-screen">
+                <div className={'z-10 row-start-1 col-start-1 ' + (className || '')}>
+                    {children}
+                </div>
+                <div
+                    className={
+                        'row-start-1 col-start-1 overflow-x-clip flex flex-col justify-center image-parent ' +
+                        (imageClassName || '')
+                    }
+                >
+                    <Image quality={90} priority placeholder="blur" {...imageProps} />
+                </div>
+            </div>
+            <style>{`
+                .image-parent * {
+                    max-width: none !important;
+                    width: auto !important;
+                    left: 50% !important;
+                    transform: translate(-50%, 0) !important;
+                }
+            `}</style>
+        </>
+    );
+}
+
+export { ImageBackground };
