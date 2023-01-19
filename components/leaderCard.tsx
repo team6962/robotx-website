7fa0fc67c585501ea68a6d1f08f0f19f71004442
Soren <soren@aethre.co>
2023-01-18 19:28:07 -0800

    Type error: Cannot find name 'StaticImageData'.

--- components/leaderCard.tsx
+++ components/leaderCard.tsx
@@ -1,31 +1,31 @@
 import Image from 'next/image';
 
 export const LeaderCard = ({
-    name,
-    title,
-    mentor = false,
-    src
+	name,
+	title,
+	mentor = false,
+	src
 }: {
-    name: string;
-    title: string;
-    mentor?: boolean;
-    src: string | StaticImageData;
+	name: string;
+	title: string;
+	mentor?: boolean;
+	src: string;
 }) => (
-    <div className="flex flex-col rounded-md background-accent">
-        <Image
-            width={500}
-            height={500}
-            className="rounded-t-md"
-            src={src}
-            alt={`${name}, ${title}`}
-            quality={50}
-            placeholder="blur"
-        />
-        <div className="flex flex-col p-2 gap-2 items-center text-center">
-            <p className="text-accent">{name}.</p>
-            <p className="text text-sm">
-                {title} {mentor && 'Student Mentor'}
-            </p>
-        </div>
-    </div>
+	<div className="flex flex-col rounded-md background-accent">
+		<Image
+			width={500}
+			height={500}
+			className="rounded-t-md"
+			src={src}
+			alt={`${name}, ${title}`}
+			quality={50}
+			placeholder="blur"
+		/>
+		<div className="flex flex-col p-2 gap-2 items-center text-center">
+			<p className="text-accent">{name}.</p>
+			<p className="text text-sm">
+				{title} {mentor && 'Student Mentor'}
+			</p>
+		</div>
+	</div>
 );
