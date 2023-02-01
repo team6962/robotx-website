f0160e71d922e4f5568f5d2d2feb05a20aeb3a4f
Soren <soren@aethre.co>
2023-02-01 10:24:39 -0800

    nicer looking members page

--- src/components/MembersPage.tsx
+++ src/components/MembersPage.tsx
@@ -3,28 +3,36 @@ import { HeadFC, PageProps } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { PageWrapper } from './PageWrapper';
 
+import * as styles from '../styles/MembersPage.module.css';
+
 export const MembersPage: React.FC<PageProps<Queries.MembersPageQuery>> = ({ data }) => {
 	const { year, leadership, members } = data.contentfulSeasonPage!;
 	return (
 		<PageWrapper>
 			<h1>{year} Roster</h1>
 			<h3>Leadership</h3>
-			<div>
+			<div className={styles.leadership}>
 				{leadership!.map((member) => (
 					<div>
-						<GatsbyImage image={member!.gatsbyImageData!} alt={member!.title!} />
-						{member!.description!.split('\n').map((line) => (
-							<p>{line}</p>
-						))}
+						<GatsbyImage
+							image={member!.gatsbyImageData!}
+							alt={member!.title!}
+							className={styles.image}
+						/>
+						<div className={styles.title}>
+							{member!.description!.split('\n').map((line) => (
+								<p>{line}</p>
+							))}
+						</div>
 					</div>
 				))}
 			</div>
-			<h3>Members</h3>
-			<div>
+			<h3>Membership</h3>
+			<p className={styles.members}>
 				{members!.map((member) => (
-					<p>{member}</p>
+					<span>{member}</span>
 				))}
-			</div>
+			</p>
 		</PageWrapper>
 	);
 };
