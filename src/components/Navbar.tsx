a94275c25c9c15f4ed317cd16961f319bf7f4428
Soren <soren@aethre.co>
2023-02-01 10:01:54 -0800

    add current season to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,16 +5,24 @@ import { Icon } from './Icon';
 import * as styles from '../styles/Navbar.module.css';
 
 export const Navbar: React.FC = () => {
-	const { allContentfulGlobalPage } = useStaticQuery<Queries.NavbarQuery>(graphql`
-		query Navbar {
-			allContentfulGlobalPage(filter: { homepage: { eq: false } }) {
-				nodes {
-					title
-					slug
+	const { allContentfulGlobalPage, allContentfulSeasonPage } =
+		useStaticQuery<Queries.NavbarQuery>(graphql`
+			query Navbar {
+				allContentfulGlobalPage(filter: { homepage: { eq: false } }) {
+					nodes {
+						title
+						slug
+					}
+				}
+				allContentfulSeasonPage(sort: { year: DESC }, limit: 1) {
+					nodes {
+						year
+					}
 				}
 			}
-		}
-	`);
+		`);
+
+	const currentSeason = allContentfulSeasonPage.nodes[0].year;
 
 	return (
 		<div className={styles.navbar}>
@@ -24,6 +32,9 @@ export const Navbar: React.FC = () => {
 				</Link>
 				<Link to="/blog">
 					<span>Blog</span>
+				</Link>
+				<Link to={`/seasons/${currentSeason}`}>
+					<span>{currentSeason}</span>
 				</Link>
 				<Link to="/seasons">
 					<span>Seasons</span>
