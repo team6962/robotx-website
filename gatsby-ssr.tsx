34dc1f8174d79670e3f8802a26d8a521737e35d8
alishaf18 <116465433+alishaf18@users.noreply.github.com>
2024-01-09 16:40:59 -0800

    Update gatsby-ssr.tsx

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -10,7 +10,7 @@ export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }: Pr
 			<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />,
 			<link rel="manifest" href="/favicon/site.webmanifest" />,
 			<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />,
-			<link rel="shortcut icon" href="/favicon/favicon.ico" />,
+			<link rel="shortcut icon" href="/favicon/antares_logo.png" />,
 			<meta name="msapplication-TileColor" content="#2b5797" />,
 			<meta name="msapplication-config" content="/favicon/browserconfig.xml" />,
 			<meta name="theme-color" content="#ffffff" />
