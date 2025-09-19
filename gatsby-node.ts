8179541715f4508f3eece05af977d0ad6a9e8617
Brian <74162951+codeNinja-1@users.noreply.github.com>
2025-09-19 16:17:37 -0700

    Try something

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,15 +1,19 @@
-const path = require(`path`)
-// Log out information after a build is done
-exports.onPostBuild = ({ reporter }) => {
-  reporter.info(`Your Gatsby site has been built!`)
-}
+import { Actions } from "gatsby";
+import path from "path";
 
-exports.createPage = async ({ actions }) => ({
-    const { createRedirect } = actions
+export const createPages = async ({ actions }: { actions: Actions }) => {
+  const { createRedirect, createPage } = actions;
 
-    createRedirect({
-    fromPath: `/aim/:id`,
+  createRedirect({
+    fromPath: `/redirect/:id`,
     toPath: `https://www.appsheet.com/start/2d6685c4-c5b4-4177-b693-28c0f739c0f7#view=Catalog_Detail&row=:id`,
-    statusCode: 302,
-  })
-})
+    isPermanent: true,
+    force: true,
+    redirectInBrowser: true
+  });
+
+  createPage({
+    path: `/redirect/*`,
+    component: path.resolve('./src/templates/redirect-template.tsx'),
+  });
+};
